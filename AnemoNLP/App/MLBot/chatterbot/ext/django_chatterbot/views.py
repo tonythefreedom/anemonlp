96b232080107dd3270a402ce7ee6f021864c315d
Lea <tony@techmaru.com>
2017-11-28 10:44:23 +0900

    장고 예제를 재 구성하였음 urls, settings, views 를 강의에 맞게 재 수정하였음

--- AnemoNLP/App/MLBot/chatterbot/ext/django_chatterbot/views.py
+++ AnemoNLP/App/MLBot/chatterbot/ext/django_chatterbot/views.py
@@ -93,7 +93,7 @@ class ChatterBotView(View):
         )
 
         # Train based on the english corpus
-        #chatbot.train("/Users/kimtony/Desktop/corpus/")
+        chatbot.train("/Users/kimtony/workspace/anemonlp/AnemoNLP/App/MLBot/chatterbot/corpus")
 
         #conversation = self.get_conversation(request)
 
--- AnemoNLP/App/MLBot/django_app/example_app/urls.py
+++ AnemoNLP/App/MLBot/django_app/example_app/urls.py
@@ -1,13 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from chatterbot.ext.django_chatterbot import urls as chatterbot_urls
-from example_app.views import ChatterBotAppView
+from views import ChatterBotView
 
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls), name='admin'),
-    url(r'^chatterbot', ChatterBotAppView.as_view(), name='main'),
-    url(r'^api/chatterbot/', include(chatterbot_urls, namespace='chatterbot')),
+    url(r'^chatterbot', ChatterBotView.as_view(), name='main'),
     url(r'^', include('learningdata_app.urls'), name = 'index'),
     url(r'^learningdata', include('learningdata_app.urls'), name = 'learning'),
 ]
